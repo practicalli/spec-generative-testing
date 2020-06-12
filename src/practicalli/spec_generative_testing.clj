02ed4795d22c2e8e678c5dfa4661659193447d2f
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-06-12 04:10:28 +0100

    Initial experiments with spec and test check

--- src/practicalli/spec_generative_testing.clj
+++ src/practicalli/spec_generative_testing.clj
@@ -1,1 +1,103 @@
-(ns practicalli.spec-generative-testing)
+(ns practicalli.spec-generative-testing
+  (:require
+   [clojure.spec.alpha :as spec]
+   [clojure.spec.gen.alpha :as spec-gen]
+   [clojure.spec.test.alpha :as spec-test]
+   [clojure.spec.test.check]))
+
+;; Set up project
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; clj-new now adds org.clojure/test.check as a dependency under the :test alias
+
+;; add a dir-locals.el file and add the :test alias when running the repl from Emacs
+;; ((clojure-mode . ((cider-clojure-cli-global-options . "-A:test"))))
+
+
+
+;; Generators
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(spec-gen/generate (spec/gen int?))
+
+(spec-gen/generate (spec/gen nil?))
+
+(spec-gen/sample (spec/gen string?))
+
+(spec-gen/sample (spec/gen #{:club :diamond :heart :spade}))
+
+(spec-gen/sample (spec/gen (spec/cat :k keyword? :ns (spec/+ number?))))
+
+
+
+
+;; Player specification
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(def suit? #{:clubs :diamonds :hearts :spades})
+(def rank? (into #{:jack :queen :king :ace} (range 2 11)))
+(spec/def ::playing-card (spec/tuple rank? suit?))
+(spec/def ::delt-hand (spec/* ::playing-card))
+
+(spec/def ::name string?)
+(spec/def ::score int?)
+(spec/def ::player (spec/keys :req [::name ::score ::delt-hand]))
+
+
+(spec/def ::deck (spec/* ::playing-card))
+(spec/def ::players (spec/* ::player))
+(spec/def ::game (spec/keys :req [::players ::deck]))
+
+
+;; generating a random player in our card game?
+
+(spec-gen/generate (spec/gen ::player))
+;; => #:practicalli.spec-generative-testing{:name "Yp34KE63vALOeriKN4cBt", :score 225, :delt-hand ([9 :hearts] [4 :clubs] [8 :hearts] [10 :clubs] [:queen :spades] [3 :clubs] [6 :hearts] [8 :hearts] [7 :diamonds] [:king :spades] [:ace :diamonds] [2 :hearts] [4 :spades] [2 :clubs] [6 :clubs] [8 :diamonds] [6 :spades] [5 :spades] [:queen :clubs] [:queen :hearts] [6 :spades])}
+
+
+
+(spec-gen/generate (spec/gen ::game))
+
+
+
+;; Specification for function definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn regulation-cards-deck
+  [{:keys [::deck ::players] :as game}]
+  (apply + (count deck)
+         (map #(-> % ::delt-hand count) players)))
+
+(defn deal-cards
+  [game]
+  game)
+
+(spec/fdef deal-cards
+  :args (spec/cat :game ::game)
+  :ret ::game
+  :fn #(= (regulation-cards-deck (-> % :args :game))
+          (regulation-cards-deck (-> % :ret))))
+
+
+
+;; Testing with spec
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; spec/check takes a fully-qualified symbol so we use ` here to resolve it in the context of the current namespace.
+
+(spec-test/check `deal-cards)
+
+;; run a specific number of tests
+
+
+(spec-test/check `deal-cards
+                 {::clojure.spec-test-check/opts {:num-tests 1}})
+
+(spec-test/check `deal-cards
+                 {:num-tests 1})
+;; => ({:spec #object[clojure.spec.alpha$fspec_impl$reify__2524 0x26debeba "clojure.spec.alpha$fspec_impl$reify__2524@26debeba"],
+;; :clojure.spec.test.check/ret
+;;  {:result true, :pass? true, :num-tests 1000, :time-elapsed-ms 75054, :seed 1591928968683},
+;; :sym practicalli.spec-generative-testing/deal-cards})
